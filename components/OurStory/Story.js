90e2b2ee736b469275b9d8620430044dbf3a54e5
Your Name <you@example.com>
2021-12-26 23:10:53 +0100

    red lips first initializ...

---
+++ components/OurStory/Story.js
@@ -0,0 +1,84 @@
+import { useState, useRef, useEffect } from "react";
+import gsap, { Expo } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+function Story({ styles }) {
+  gsap.registerPlugin(ScrollTrigger);
+  const story = useRef(null);
+  useEffect(() => {
+    const panels = gsap.utils.toArray(".panel");
+    gsap.to(panels, {
+      xPercent: -100 * (panels.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".wrappingIT",
+        pin: true,
+        start: "top top",
+        scrub: 1,
+        snap: {
+          snapTo: 1 / (panels.length - 1),
+          inertia: false,
+          duration: { min: 2, max: 2 },
+        },
+        end: () => "+=" + document.querySelector(".wrappingIT").offsetWidth,
+      },
+    });
+  }, []);
+  //this second useRffect is used  to contrôl scrollEffect for each section
+  const Firstyear = useRef(null);
+  const storyFirstImage = useRef(null);
+  const storySecondImage = useRef(null);
+  const storyLastImage = useRef(null);
+  //const Firstyear = useRef(null)
+  useEffect(() => {
+    const tl = gsap.timeline({
+      ease: Expo.easeOut,
+      delay: 0.2,
+      duration: 2,
+      scrollTrigger: {
+        trigger: Firstyear.current,
+        start: "top 100%",
+        scrub: 1,
+        markers:{
+            startColor:"yellow",
+            endColor:"yellow",
+            fontSize:"40px"
+        }
+      },
+    });
+    tl.to(storyFirstImage.current, { y: 23 })
+    tl.to(storyFirstImage.current.children[0], { scale:1.5 })
+    tl.to(storySecondImage.current, { y: 23 })
+    tl.to(storySecondImage.current.children[0], { scale:1.5 })
+    tl.to(storyLastImage.current, { y: 23 })
+    tl.to(storyLastImage.current.children[0], { scale:1.5 })
+  }, []);
+  return (
+    <section ref={story} className={`wrappingIT ${styles._story}`}>
+      <div ref={Firstyear} className={`panel ${styles.firstYear}`}>
+        <div className={styles.redLikeblood}>Red Like Blood</div>
+        <div className={styles.firstYear_Images}>
+          <div ref={storyFirstImage}>
+            <img src="/storyFirstYear.jpg" alt="" />
+          </div>
+          <div ref={storySecondImage}>
+            <img src="/storyFirstYear2.jpg" alt="" />
+          </div>
+          <div ref={storyLastImage}>
+            <img src="/storyFirstYear3.jpg" alt="" />
+          </div>
+        </div>
+        <div className={styles.firstYear_text}>
+          <p>“If you’re sad, add more lipstick and attack.”</p>
+        </div>
+      </div>
+      <div className={`panel ${styles.secondYear}`}>
+        <div></div>
+      </div>
+      <div className={`panel ${styles.today}`}>
+        <div></div>
+      </div>
+    </section>
+  );
+}
+
+export default Story;
---
+++ components/hero/Hero.js
@@ -0,0 +1,92 @@
+import { useEffect, useRef } from "react";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import redLips from "../../public/great.jpg";
+import gsap, { Expo } from "gsap";
+import SplitText from "../../utils/Split3.min.js";
+function Hero({ styles }) {
+  const Main_Title = useRef(null);
+  const leftTitle = useRef(null);
+  const HeroImg = useRef(null);
+  const rightTitle = useRef(null);
+  const inDoubt = useRef(null);
+  useEffect(() => {
+    const split = new SplitText(Main_Title.current, {
+      type: "lines",
+      linesClass: "lineChildren",
+    });
+    const splitParent = new SplitText(Main_Title.current, {
+      type: "lines",
+      linesClass: "lineParent",
+    });
+    const tl = gsap.timeline({ defaults: { delay: 0.2, duration: 1.5 } });
+    tl.fromTo(inDoubt.current, { opacity: 0 }, { opacity: 1 })
+      .fromTo(
+        inDoubt.current,
+        { position: "absolute", top: "50%" },
+        { position: "absolute", top: "5%" }
+      )
+      .fromTo(
+        inDoubt.current,
+        { position: "absolute", right: "50%" },
+        { position: "absolute", right: "20%" }
+      )
+      .to(split.lines, {
+        y: 0,
+        opacity: 1,
+        stagger: 0.1,
+        ease: Expo.easeOut,
+      })
+      .fromTo(
+        HeroImg.current,
+        {
+          clipPath: "polygon(0 0 , 0 0, 30% 100% , 0% 100%)",
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          clipPath: "polygon(0 0 , 100% 0, 100% 100% , 0% 100%)",
+        }
+      )
+      .fromTo(leftTitle.current, { opacity: 0, x: -500 }, { x: 50, opacity: 1 })
+      .fromTo(
+        rightTitle.current,
+        { x: 500, opacity: 0 },
+        { x: -150, opacity: 1 }
+      )
+      .to(HeroImg.current.children[0], { scale: 1.5 });
+  }, []);
+  useEffect(() => {
+    const rotatedText = inDoubt.current;
+    rotatedText.innerHTML = rotatedText.textContent.replace(
+      /\S/g,
+      '<span class="rotatedSpan">$&</span>'
+    );
+    const rotatedSpans = document.querySelectorAll(".rotatedSpan");
+    rotatedSpans.forEach((letter, i) => {
+      letter.style.transform = "rotate(" + i * 15 + "deg)";
+    });
+  }, []);
+  return (
+    <section className="hero">
+      <div className={styles._hero}>
+        <h1 ref={Main_Title}>Red Lips</h1>
+        <div className={styles._hero_Child}>
+          <div ref={leftTitle} className={styles.left_title}>
+            <span>Life's better in</span>
+          </div>
+          <div ref={HeroImg} className={styles._hero_container_Img}>
+            <img src={redLips} width={840} height={440} alt="" />
+          </div>
+          <div ref={rightTitle} className={styles.right_title}>
+            <span>red lipstick.</span>
+          </div>
+          <div ref={inDoubt} className={styles.in_Doubt}>
+            When - in - doubt - wear - red
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
